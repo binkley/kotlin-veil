2fb4ea2206fe7b62b89fe63ad865c2aaaa9b0de2
Brian Oxley <boxley@thoughtworks.com>
2020-05-20 16:19:07 -0500

    Fix project naming

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-jar=target/kotlin-rational-1.0.1-SNAPSHOT-jar-with-dependencies.jar
+jar=target/kotlin-veil-1.0.1-SNAPSHOT-jar-with-dependencies.jar
 
 test -r $jar || ./mvnw -C package
 
