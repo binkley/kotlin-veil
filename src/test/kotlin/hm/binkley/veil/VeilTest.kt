7e6da9b3b9dd83c9541f1ccea7089f0866a7e5a4
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-05-31 09:01:20 -0500

    Cleaner test code

--- src/test/kotlin/hm/binkley/veil/VeilTest.kt
+++ src/test/kotlin/hm/binkley/veil/VeilTest.kt
@@ -1,6 +1,7 @@
 package hm.binkley.veil
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 private const val veiledRowOneA = 2
@@ -10,80 +11,91 @@ private const val realRowOneB = "apple"
 private val fakeDs = FakeBobDataSource(-1, realRowOneB)
 
 internal class VeilTest {
-    @Test
-    fun `should veil first time, and stay veiled`() {
-        fakeDs.rowOneA = veiledRowOneA
+    @Nested
+    inner class Veiling {
+        @Test
+        fun `should veil first time, and stay veiled`() {
+            fakeDs.rowOneA = veiledRowOneA
 
-        val bobs = bobs().pierceable
-        // Sequences are not restartable -- TODO: Use List?
-        val bobOne = bobs.first()
+            val bobs = bobs().pierceable
+            // Sequences are not restartable -- TODO: Use List?
+            val bobOne = bobs.first()
 
-        assertEquals(veiledRowOneA, bobOne.a)
+            assertEquals(veiledRowOneA, bobOne.a)
 
-        fakeDs.rowOneA = piercedRowOneA // No effect
+            fakeDs.rowOneA = piercedRowOneA // No effect
 
-        assertEquals(veiledRowOneA, bobOne.a)
+            assertEquals(veiledRowOneA, bobOne.a)
+        }
+
+        @Test
+        fun `should pierce second time when pierceable`() {
+            fakeDs.rowOneA = veiledRowOneA
+
+            val bobs = bobs().pierceable
+            // Sequences are not restartable -- TODO: Use List?
+            val bobOne = bobs.first()
+
+            fakeDs.rowOneA = piercedRowOneA
+            bobOne.b // Pierce the veil with an unveiled property
+
+            assertEquals(piercedRowOneA, bobOne.a)
+        }
+
+        @Test
+        fun `should not veil when not veilable`() {
+            val bobs = bobs().pierceable
+            // Sequences are not restartable -- TODO: Use List?
+            val bobOne = bobs.first()
+
+            assertEquals(realRowOneB, bobOne.b)
+        }
+
+        @Test
+        fun `should not pierce second time when unpierceable`() {
+            fakeDs.rowOneA = veiledRowOneA
+
+            val bobs = bobs().unpierceable
+            // Sequences are not restartable -- TODO: Use List?
+            val bobOne = bobs.first()
+
+            bobOne.b // Pierce the veil
+            fakeDs.rowOneA = piercedRowOneA
+
+            assertEquals(veiledRowOneA, bobOne.a)
+        }
     }
 
-    @Test
-    fun `should pierce second time when pierceable`() {
-        fakeDs.rowOneA = veiledRowOneA
+    @Nested
+    inner class Reflecting {
+        @Test
+        fun `should reflect`() {
+            val bobs = bobs().pierceable
+            // Sequences are not restartable -- TODO: Use List?
+            val bobOne = bobs.first()
 
-        val bobs = bobs().pierceable
-        // Sequences are not restartable -- TODO: Use List?
-        val bobOne = bobs.first()
+            @Suppress("UNCHECKED_CAST")
+            bobOne as Veilable<Bob>
 
-        fakeDs.rowOneA = piercedRowOneA
-        bobOne.b // Pierce the veil with an unveiled property
+            assertEquals(false, bobOne.pierced)
+            assertEquals(true, bobOne.veiled(Bob::a))
+            assertEquals(false, bobOne.veiled(Bob::b))
+            assertEquals(false, bobOne.veiled(Bob::veiled))
+        }
 
-        assertEquals(piercedRowOneA, bobOne.a)
-    }
+        /**
+         * Key point: `veiled` is _both_ a prop on the data object, and a
+         * reflective property (using JDK proxy logic).  The object prop
+         * should not be hidden by the reflective prop.
+         */
+        @Test
+        fun `should not hide props because of reflection`() {
+            val bobs = bobs().unpierceable
+            // Sequences are not restartable -- TODO: Use List?
+            val bobOne = bobs.first()
 
-    @Test
-    fun `should not veil when not veilable`() {
-        val bobs = bobs().pierceable
-        // Sequences are not restartable -- TODO: Use List?
-        val bobOne = bobs.first()
-
-        assertEquals(realRowOneB, bobOne.b)
-    }
-
-    @Test
-    fun `should not pierce second time when unpierceable`() {
-        fakeDs.rowOneA = veiledRowOneA
-
-        val bobs = bobs().unpierceable
-        // Sequences are not restartable -- TODO: Use List?
-        val bobOne = bobs.first()
-
-        bobOne.b // Pierce the veil
-        fakeDs.rowOneA = piercedRowOneA
-
-        assertEquals(veiledRowOneA, bobOne.a)
-    }
-
-    @Test
-    fun `should reflect`() {
-        val bobs = bobs().pierceable
-        // Sequences are not restartable -- TODO: Use List?
-        val bobOne = bobs.first()
-
-        @Suppress("UNCHECKED_CAST")
-        bobOne as Veilable<Bob>
-
-        assertEquals(false, bobOne.pierced)
-        assertEquals(true, bobOne.veiled(Bob::a))
-        assertEquals(false, bobOne.veiled(Bob::b))
-        assertEquals(false, bobOne.veiled(Bob::veiled))
-    }
-
-    @Test
-    fun `should not hide props because of reflection`() {
-        val bobs = bobs().unpierceable
-        // Sequences are not restartable -- TODO: Use List?
-        val bobOne = bobs.first()
-
-        assertEquals(17, bobOne.veiled)
+            assertEquals(17, bobOne.veiled)
+        }
     }
 }
 
